79aeeeb30816c8ab23f0e9fc2207468d4ed8c49b
Akash <59533027+akash-pixel@users.noreply.github.com>
2022-01-04 13:34:44 +0530

    Update QuickSort_2.java
    Deviding every element in to separate arraylist and then combining them recursively.

--- QuickSort_2.java
+++ QuickSort_2.java
@@ -3,49 +3,72 @@ import java.lang.*;
 
 public class QuickSort_2 {
 
-    public static void printArr(int[] arr) {
-        for(int i=0; i<arr.length; i++){
-            System.out.print( arr[i]+" " );
+    public static void printArr(ArrayList<Integer> list){
+        for(int item : list){
+            System.out.print(item+" ");
         }
-    }
-
-    private static void Swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[low];
-        int i= high-1;
-
-        for(int j=low; j<=high-1; j++){
-            if(arr[j]< pivot){
-                 i++;
-                 Swap(arr, i,j);
-            }
-        }
-        Swap(arr, i+1, high);
-
-        return (i+1);
-    }
-
-    public static void QuickSort(int[] arr, int low, int high) {
-        if(low < high){
-            int pi = partition(arr, low, high);
-            
-           // printArr(arr);
-            
-            QuickSort(arr, low, pi-1);
-            QuickSort(arr, pi+1, high);
-        }
-    } //end
-
-    public static void main(String[] args) {
-        int n=5; 
-        int[] arr={5, 8, 1, 3, 7, 9, 2};
-
-        QuickSort(arr, 0, n-1);
+        System.out.println();
         return;
     }
+    
+    // Main function
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		//int[] arr = {5,8,1,3,7,9,2};
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<n; i++){
+		    int num = sc.nextInt();
+		    list.add(num);
+		}
+		
+		
+		list = Sort(list);
+		//printArr(list);
+	}
+	
+	// Devideing all the elements into arraylist
+    public static ArrayList<Integer> Sort(ArrayList<Integer> list){
+        if(list.size() <=1 ){ return list;}
+        
+        
+            ArrayList<Integer> low = new ArrayList<>();
+            ArrayList<Integer> high = new ArrayList<>();
+            
+            int m = list.get(0);
+            list.remove(0);
+            
+            for(int i: list){
+                if(i<m){
+                    low.add(i);
+                }else{
+                    high.add(i);
+                }
+            }
+            
+            
+            //if(low.size()>1){
+                low = Sort(low );
+            //}
+            //if(high.size()>1 ){
+                high = Sort(high);
+            //}   
+            
+            return Merge(low, m, high);
+            
+            
+    }
+    
+    // Merging all the array lists
+    public static ArrayList<Integer> Merge(ArrayList<Integer> low, int m , ArrayList<Integer> high){
+        low.add(m);
+        for(int i: high){
+            low.add(i);
+        }
+        printArr(low);
+        return low;
+    }
+    
 }
